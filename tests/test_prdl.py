b4dba5a8e23c28985f5afa9871f9cd54ab900d2f
Bohdan Bobrowski <bohdanbobrowski@gmail.com>
2025-12-16 09:02:33 +0100

    Fixes and refactor

--- tests/test_prdl.py
+++ tests/test_prdl.py
@@ -11,6 +11,24 @@ class TestDefaultCrawler:
         # Then
         assert len(result) > 0
 
+    def test_audiobook(self):
+        # Given
+        with open("./tests/data/audiobook.html") as f:
+            given_page_data = f.read()
+        # When
+        result = PrDlCrawl._get_podcasts_v2(given_page_data)
+        # Then
+        assert len(result) > 0
+
+    def test_podcasts_episodes_escaped(self):
+        # Given
+        with open("./tests/data/podcasts_episodes_escaped.html") as f:
+            given_page_data = f.read()
+        # When
+        result = PrDlCrawl._get_podcasts_v2(given_page_data)
+        # Then
+        assert len(result) > 0
+
     def test_podcasts_escaped(self):
         # Given
         with open("./tests/data/page1.html") as f:
@@ -26,35 +44,17 @@ class TestDefaultCrawler:
         assert result[3].file_name == "mroczna-historia-watykanu.mp3"
         assert len(result) == 10
 
-    def test_podcasts_episodes_escaped(self):
-        # Given
-        with open("./tests/data/podcasts_episodes_escaped.html") as f:
-            given_page_data = f.read()
-        # When
-        result = PrDlCrawl._get_podcasts_v2(given_page_data)
-        # Then
-        assert len(result) > 0
-
-    def test_prdl_get_podcasts_v2_page3(self):
-        # Given
-        with open("./tests/data/page3.html") as f:
-            given_page_data = f.read()
-        # When
-        result = PrDlCrawl._get_podcasts_v2(given_page_data)
-        # Then
-        assert len(result) > 0
-
-    def test_prdl_get_podcasts_v2_jedynka(self):
+    def test_jedynka(self):
         """url: https://jedynka.polskieradio.pl/artykul/810503,Weterani-powstania-czyli-oczko-w-g%C5%82owie-Pi%C5%82sudskiego-"""
         # Given
-        with open("./tests/data/page4.html") as f:
+        with open("./tests/data/jedynka.html") as f:
             given_page_data = f.read()
         # When
         result = PrDlCrawl._get_podcasts_v2(given_page_data)
         # Then
         assert len(result) > 0
 
-    def test_prdl_get_podcasts_data_media(self):
+    def test_polskieradio24(self):
         """url: https://polskieradio24.pl/artykul/2794550,bitwa-pod-tannenbergiem-najdotkliwsza-kleska-rosji-w-wielkiej-wojnie"""
         # Given
         with open("./tests/data/page5.html") as f:
